1abe44d633742c985025a815fbd05b127ea42658
Princep <fattakhov.rr@phystech.edu>
2023-04-06 01:54:41 +0300

    continue to write GUI

--- src/GUI/Widget.hpp
+++ src/GUI/Widget.hpp
@@ -1,27 +1,100 @@
 #ifndef WIDGET_HPP
 #define WIDGET_HPP
 
-#include "gui.hpp"
+#include "gui_lib.hpp"
+#include "Pixel.hpp"
+//#include "GrShapes.hpp"
 #include <iostream>
 
 namespace gui
 {
-
-    class Widget
+    class BoundingBox
     {
     private:
-        std::vector<pixel> pixels;
+        sf::Vector2f location;
+        sf::Vector2f size;
 
     public:
-        //void draw() const; Managers role
-        std::vector<pixel> getPixels() const {
-            return pixels;
-        }
-        virtual void fillPixels() = 0;
         
-        void close();
     };
 
+    class Widget // : public GrShape
+    {
+    private:
+        Widget* const canvas_ptr;
+        const int type;
+
+    public:
+        Widget(Widget* const canvas_ptr_, const int type_):
+        canvas_ptr(canvas_ptr_), type(type_)
+        {};
+    
+    public:
+        struct HandlerInfo
+        {
+            bool catched = false;
+            bool forParent = false;
+            int signal = DEFAULT;
+        };
+        virtual HandlerInfo catchEvent(const sf::Event event) = 0;
+    
+    public:
+        const int type() const {return type;}
+        enum TYPES
+        {
+            SLAVE,
+            MASTER,
+        };
+    };
+
+    class WidgetManager: public Widget
+    {
+    private:
+        std::vector<Widget*> widgets;
+
+    private:
+        virtual void reqeustToSignal(int signal);
+        enum SIGNALS
+        {
+            DEFAULT = 0,
+        };
+
+    public:
+
+    
+    public:
+        HandlerInfo catchEvent(const sf::Event event)
+        {
+            HandlerInfo input_info;
+            HandlerInfo output_info;
+
+            for (const auto& widget_ptr : widgets)
+            {
+                input_info = widget_ptr->catchEvent(event);
+                if (input_info.catched = false)
+                {
+                    continue;
+                }
+                else
+                {
+                    output_info.catched = true;
+                    if (widget_ptr->type() == SLAVE)
+                    {
+                        if (input_info.forParent = false)
+                        {
+                            break;
+                        }
+                        else{
+                            reqeustToSignal(input_info.signal); 
+                        }
+                    }
+                    
+                }
+            }
+
+            return output_info;
+        }
+    };
 }
 
 #endif
--- src/GUI/gui.hpp
+++ src/GUI/gui.hpp
@@ -1,7 +1,7 @@
 #ifndef GUI_HPP
 #define GUI_HPP
 
-#include <SFML/Graphics.hpp>
+#include "gui_lib.hpp"
 
 namespace gui
 {
@@ -10,48 +10,6 @@ namespace gui
         WINDOW_WIDTH = 1200,
         WINDOW_HEIGHT = 800    
     };
-
-    class pixel : private sf::RectangleShape
-    {
-    public:
-        pixel(const sf::Vector2f position, const sf::Color color)
-        {   
-            setSize({1.f, 1.f});
-            setPosition(position);
-            setFillColor(color);
-        };
-    public:
-        using RectangleShape::getFillColor;
-        using RectangleShape::setFillColor;
-        using RectangleShape::getPosition;
-        using RectangleShape::setPosition;
-    };
-
-    class WManager: private sf::RenderWindow
-    {
-    private:
-        Layer layers[2];
-        //sf::RenderWindow& window;
-    public:
-        WManager(const int w_width, const int w_height, const char* title)
-        {
-            RenderWindow(sf::VideoMode(w_width, w_height), title);
-        }
-    };
-
-    class Mouse
-    {
-
-    };
-
-    class Layer
-    {
-    private:
-        std::vector<Widget> Widgets_;
-
-    public:
-
-    };
-};
+}
 
 #endif
